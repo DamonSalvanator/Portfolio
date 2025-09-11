cedffb6112d4f4192f6587baeb3eed7b380bb1c1
DamonSalvanator <yashbagde2004july@gmail.com>
2025-09-11 19:00:41 +0530

    third commit

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname,"./frontend/dist")))
 app.use('/api/v1/portfolio',require("./Routes/Routes"))
 
 
-app.all('*splat',(req,res)=>{
+app.all('*',(req,res)=>{
     res.sendFile(path.join(__dirname,"./frontend/dist/index.html"));
 })
 
